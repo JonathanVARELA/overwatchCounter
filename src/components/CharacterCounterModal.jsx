d1cdfc0db4ae3b26e0a14e627db7feaa6f405340
jonathan VARELA <jonathan.varela@zenika.com>
2020-04-30 11:22:15 +0200

    pretty scrollbar

---
+++ src/components/CharacterCounterModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./CharacteerCounterModal.css"
+
+const CharacterCounterModal = () => {
+
+    return (
+        <div className={"character-counter-modal"}>
+        </div>
+    )
+};
+
+export default CharacterCounterModal;
